e54b81cb2077252d6fa693f2ae88dc8d326d032f
Vipul Varshney <vipulvarshney1@gmail.com>
2023-11-05 14:35:55 +0530

    initial commit .net WebApplication7

---
+++ tushar_store_product_list.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="tushar_store_product_list.aspx.cs" Inherits="WebApplication7.tushar_store_product_list" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Product Name :</td>
+                    <td><asp:TextBox ID="text_pd_name" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Product Expiry :</td>
+                    <td><asp:TextBox ID="text_pd_expiry" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Product Reck Number :</td>
+                    <td><asp:TextBox ID="text_pd_reck_no" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:Button ID="btninsert" runat="server" Text="submit" OnClick="btninsert_Click" /> </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:GridView ID="gv_store" runat="server"></asp:GridView></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ tushar_store_product_list.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication7
+{
+    public partial class tushar_store_product_list : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data source=DESKTOP-1J5P8LQ\\SQLEXPRESS;Initial catalog=tushar_stores;Integrated security=true");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            display();
+        }
+        public void display()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from tushar_products", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            gv_store.DataSource = dt;
+            gv_store.DataBind();
+        }
+
+        protected void btninsert_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into tushar_products(pd_name,pd_expiry,pd_reck_number)values('"+ text_pd_name + "','"+ text_pd_expiry + "','"+ text_pd_reck_no + "')",con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            display();
+        }
+    }
+}
